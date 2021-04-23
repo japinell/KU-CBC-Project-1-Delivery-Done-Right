acb04900686cdb480672da99ebd9b58bd8155690
Travis Brown <bergannation@gmail.com>
2021-04-22 20:18:16 -0500

    JS test push

--- Assets/js/javascript.js
+++ Assets/js/javascript.js
@@ -1,6 +1,3 @@
-//
-const NUT_API_SERVER = "https://api.nutritionix.com";
-const NUT_API_END_POINT = "/v1-1/search";
 const NUT_API_KEY = "dcb0f2394e224dd571c331c9f7960d2a";
 const NUT_API_ID = "3a3f663b";
 // Elements
@@ -58,3 +55,5 @@ function addSearchHistory(searchCity) {
   }
   //
 }
+
+// Test Comment
