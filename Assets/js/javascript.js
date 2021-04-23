a1aadc1ac082bca8f80c1110a0d12d0b78640f03
JAPinell <japinell@yahoo.com>
2021-04-22 20:16:58 -0500

    Added constants for NUT API

--- Assets/js/javascript.js
+++ Assets/js/javascript.js
@@ -1,4 +1,5 @@
 // Constants
+// Another comment here
 const NUT_API_SERVER = "https://api.nutritionix.com";
 const NUT_API_END_POINT = "/v1-1/search";
 const NUT_API_KEY = "dcb0f2394e224dd571c331c9f7960d2a";
