75a081fe356fab741d31351d7a560378ecef9056
jtstockard <jtstockard92@gmail.com>
2021-04-27 21:11:58 -0500

    updated the search render to include common item id and calories

--- nutritionsjoel.js
+++ nutritionsjoel.js
@@ -1,10 +1,9 @@
 // Constants
 const NUT_API_SERVER = "https://api.nutritionix.com";
 const NUT_API_END_POINT = "/v2/search/instant";
-const CREDENTIALS = {
+const credentials = {
   "x-app-id": "bd961765",
   "x-app-key": "d4c8c6000b2d32f39325a13e6b36af32",
-  "x-remote-user-id": 0,
 };
 // Elements
 const topDiv = $("#topDiv");
@@ -13,194 +12,84 @@ const inputDiv = $("#inputDiv");
 const buttonDiv = $("#results");
 const searchButton = $("#searchButton");
 
-var searchInput = $(searchButton).data;
-
-for (i = 0; i < localStorage.length; i++);
-var foodQuery = localStorage.getItem(i);
-
-if ((searchInput = "")) {
-  console.log(searchInput);
-} else {
-  fetch(`https://trackapi.nutritionix.com/v2/search/instant?query=${food_name}`, {
-    headers: {
-      "x-app-id": "bd961765",
-      "x-app-key": "d4c8c6000b2d32f39325a13e6b36af32",
-    },
-    method: "GET",
-  }).then(function (response) {
-    var food = response.data.foods[0];
-    if (food) {
-      food.display_name = $filter("ucwords")(food.metadata.original_input || food.food_name);
-      food.append(bottomDiv);
-    }
-    return food;
-  });
-}
-/*(function () {
-  "use strict";
-
-  config.$inject = ["$stateProvider", "baseUrl", "locales"];
-  angular.module("food", ["ja.qr", "foods.locale", "localization"]).config(config);
-
-  function config($stateProvider, baseUrl, locales) {
-    $stateProvider.state("site.food", {
-      url:
-        "/{locale:" +
-        locales
-          .map(function (l) {
-            return l.url;
-          })
-          .join("|") +
-        "}/food/{raw:raw}/:natural_query/:serving",
-      params: {
-        raw: { value: null, squash: true },
-        locale: { value: null, squash: true },
-        serving: { value: null, squash: true },
-      },
-      metaTags: {
-        title: "Calories in {{item.display_name}}",
-        description:
-          "Calories, fat, protein, and carbohydrate values for for {{item.display_name}} and other related foods.",
-        properties: {},
-      },
-      templateUrl: baseUrl + "/nix_app/food/food.html",
-      controller: "foodCtrl as vm",
-      resolve: {
-        diets: [
-          "claimsPromise",
-          function diets(claimsPromise) {
-            return claimsPromise;
-          },
-        ],
-        taxonomy: [
-          "taxonomyPromise",
-          function taxonomy(taxonomyPromise) {
-            return taxonomyPromise;
-          },
-        ],
-        item: [
-          "nixTrackApiClient",
-          "$stateParams",
-          "$filter",
-          "$rootScope",
-          "GroceryFactory",
-          function item(nixTrackApiClient, $stateParams, $filter, $rootScope, GroceryFactory) {
-            if (!$stateParams.natural_query) {
-              return null;
-            }
-            var query = $stateParams.natural_query;
-            if ($stateParams.serving) {
-              var serving = $stateParams.serving
-                .split("-")
-                .filter(function (word) {
-                  return word.length === 1 || query.indexOf(word) === -1;
-                })
-                .join("-");
-              query = serving + " " + query;
-            }
-
-            return nixTrackApiClient.natural
-              .nutrients({
-                query: query.replace(/-/g, " "),
-                include_subrecipe: true,
-                use_raw_foods: !!$stateParams.raw,
-                line_delimited: true,
-                locale: _.get(
-                  _.find(locales, { label: ($stateParams.locale || "").toUpperCase() }),
-                  "lc"
-                ),
-                claims: true,
-                taxonomy: true,
-              })
-              .then(function (response) {
-                var food = response.data.foods[0];
-                if (food) {
-                  food.display_name = $filter("ucwords")(
-                    food.metadata.original_input || food.food_name
-                  );
-
-                  if (food.sub_recipe) {
-                    var subRecipeWeight = food.sub_recipe.reduce(function (total, subFood) {
-                      return total + subFood.serving_weight;
-                    }, 0);
-                    var factor = food.serving_weight_grams / subRecipeWeight;
-
-                    food.sub_recipe.forEach(function (subFood) {
-                      subFood.calories = subFood.calories * factor;
-                      subFood.serving_weight = subFood.serving_weight * factor;
-                      subFood.serving_qty = subFood.serving_qty * factor;
-                    });
-                  }
-
-                  return food;
-                }
-              })
-              .then(function (food) {
-                if (food.source === 1) {
-                  return GroceryFactory.getTagData(food.tags.tag_id)
-                    .then(function (tagData) {
-                      food.photo.thumb = tagData.tag_image;
-                      food.photo.attribution_url = tagData.tag_image_attribution_url;
-                      food.photo.user_name = tagData.tag_image_creative_commons_user_name;
-                      food.photo.license_type = tagData.tag_image_license_type;
-                    })
-                    .then(function () {
-                      return food;
-                    })
-                    ["catch"](function () {
-                      return food;
-                    });
-                }
-
-                return food;
-              })
-              .then(function (food) {
-                if (!food.photo.license_type) {
-                  food.photo = {
-                    thumb: $rootScope.images.apple.grey,
-                    highres: $rootScope.images.apple.grey,
-                  };
-                }
-
-                return food;
-              })
-              ["catch"](function () {
-                return null;
-              });
-          },
-        ],
-        publicLists: [
-          "item",
-          "nixTrackApiClient",
-          function publicLists(item, nixTrackApiClient) {
-            return (
-              item &&
-              nixTrackApiClient("/search/public_lists", {
-                params: {
-                  food: item.food_name,
-                  limit: 4,
-                },
-              })
-                .then(function (response) {
-                  return response.data.lists;
-                })
-                ["catch"](function () {
-                  return null;
-                })
-            );
-          },
-        ],
-      },
-      onEnter: [
-        "$anchorScroll",
-        function onEnter($anchorScroll) {
-          $anchorScroll();
-        },
-      ],
+$.ajax({
+  url: "https://trackapi.nutritionix.com/v2/locations",
+  headers: credentials,
+  method: "GET",
+  contentType: "application/json",
+  data: {
+    ll: zip[0].lat + "," + zip[0].lon,
+    distance: "5km",
+    limit: 20,
+  },
+})
+  .then(function (response) {
+    var brand_ids = response.locations.map(function (location) {
+      return location.brand_id;
+    });
+​
+    console.log(response);
+​
+    return $.ajax({
+      url: "https://trackapi.nutritionix.com/v2/search/instant",
+      headers: credentials,
+      method: "GET",
+      contentType: "application/json",
       data: {
-        cssClass: "bottomDiv",
+        query: queryInput || "salad",
+        branded: true,
+        self: false,
+        common: true,
+        brand_ids: JSON.stringify(brand_ids),
       },
     });
-  }
-})();
-("use strict"); */
+  })
+  .then(function (data) {
+    console.log(data);
+  
+    for (var i=0, l = data.branded.length; i < l; i++) {
+      brandedObj = {
+        restaurantName: "",
+        restaurantItemName: "",
+        foodName: "",
+        foodCalories: "",
+        nixItemId: "",
+        photoEl: "",
+        servingSize: "",
+        servingUnit: "",
+      };
+      brandedObj.restaurantName = data.branded[i].brand_name;
+      brandedObj.restaurantItemName = data.branded[i].brand_name_item_name;
+      brandedObj.foodName = data.branded[i].food_name;
+      brandedObj.foodCalories = data.branded[i].nf_calories;
+      brandedObj.nixItemId = data.branded[i].nix_item_id;
+      brandedObj.photoEl = data.branded[i].photo.thumb;
+      brandedObj.servingSize = data.branded[i].serving_qty;
+      brandedObj.servingUnit = data.branded[i].serving_unit;
+
+      renderBrandedNutrition(brandedObj)
+    }
+    for (var i = 0, l =data.common.length; i < l; i++) {
+      commonObj = {
+        foodName: "",
+        foodCalories: "",
+        nixItemId: "",
+        photoEl: "",
+        servingSize: "",
+        servingUnit: "",
+      };
+      commonObj.foodName = data.common[i].food_name;
+      commonObj.foodCalories = data.common[i].nf_calories;
+      commonObj.nixItemId = data.common[i].nix_item_id;
+      commonObj.photoEl = data.common[i].photo.thumb;
+      commonObj.servingSize = data.common[i].serving_qty;
+      commonObj.servingUnit = data.common[i].serving_unit;
+
+      renderCommonNutrition(commonObj);
+    }
+  })
+  .catch(function (response) {
+    console.error(response);
+    $("#Results").html().response;
+  });
+
